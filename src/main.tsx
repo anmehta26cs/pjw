e609a752e2db140c143523f5166784a06e15aac5
anmehta26 <anmehta26@utexas.edu>
2023-04-26 15:50:52 -0500

    basic structure of router added

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,38 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Error from './pages/Error'
+import Home from './pages/Home'
+import About from './pages/About'
+import Team from './pages/Team'
+import JoinUs from './pages/JoinUs'
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
     errorElement: <Error />,
-
   },
+  {
+    path: "/about",
+    element: <About />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/team",
+    element: <Team />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/join",
+    element: <JoinUs />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+    errorElement: <Error />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
